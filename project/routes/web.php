1cbc1624bc7d626d4c58c077be57abf939d68033
VanHuyen193 <vanhuyen190304@gmail.com>
2025-01-01 20:42:06 +0700

    delete

--- project/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\BookController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/book', [BookController::class, 'index'])->name('');
